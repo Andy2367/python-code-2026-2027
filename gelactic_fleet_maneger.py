0d472bfa5561c38d47bf17a5737d254180bbdc51
Andy2367 <afcowie@icloud.com>
2026-02-27 15:37:18 +0000

    Add files via upload

--- gelactic_fleet_maneger.py
+++ gelactic_fleet_maneger.py
@@ -1,5 +1,7 @@
+import time
+
 class spaceship:
-    def __init__(self, name , nfule, wfule , fbr, norm_dist, warp_dist):
+    def __init__(self, name , nfule, wfule , fbr, norm_dist, warp_dist,):
         self.name = name
         self.nfule = nfule
         self.wfule = wfule
@@ -13,8 +15,33 @@ class spaceship:
     def travel_warp(self):
         self.wfule = self.wfule -(self.fbr * self.warp_dist)
         return print(f"{self.wfule} units of warp fule remaneing")
+    def sustain_damege(self):
+        print("takeing damege")
 
 class fighter(spaceship):
-    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist):
+    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, damege):
+        super().__init__(name, nfule, wfule, fbr, norm_dist, warp_dist,)
+        self.damege = damege
+    def attack(self):
+        target = input("Entre your target: ")
+        print("Attacking...")
+        time.sleep(1.5)
+        print(f"{target} has sustaind {self.damege} damege")
+
+class transport(spaceship):
+    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, cargo):
         super().__init__(name, nfule, wfule, fbr, norm_dist, warp_dist)
-    def travel_norm(self): # add rest of func
+        self.cargo = cargo # expecting a list
+    def deliver_cargo(self):
+        if not self.cargo:
+            print("no cargo to deliver")
+        else:
+            print("delivering ", self.cargo[0])
+            self.cargo.pop(0)
+    def recive_cargo(self):
+        new_cargo = input("Entre what you want to add to your cargo: ")
+        print(F"{new_cargo} added to your cargo.")
+        self.cargo.append(new_cargo)
+
+xf355 = fighter("bigg bessy", 1000, 200, 1.5, 100, 4, 50)
+xf355.attack()
