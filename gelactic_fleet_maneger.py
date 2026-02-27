58f89356dc1abe8905b052a14f30c90ef83acf48
Andy2367 <afcowie@icloud.com>
2026-02-27 15:38:55 +0000

    Add files via upload

--- gelactic_fleet_maneger.py
+++ gelactic_fleet_maneger.py
@@ -1,7 +1,7 @@
 import time
-
+import random as rand
 class spaceship:
-    def __init__(self, name , nfule, wfule , fbr, norm_dist, warp_dist,):
+    def __init__(self, name , nfule, wfule , fbr, norm_dist, warp_dist):
         self.name = name
         self.nfule = nfule
         self.wfule = wfule
@@ -11,37 +11,107 @@ class spaceship:
     def travel_norm(self):
         print("traveling")
         self.nfule = self.nfule - (self.fbr * self.norm_dist)
-        return print(f"{self.nfule} units of normal fule remaneing")
+        return print(f"{self.nfule} units of normal fule remaneing.")
     def travel_warp(self):
+        dist = float(input("how far do you want to go in lightyears? e.g. 2.5"))
+        self.warp_dist = dist
         self.wfule = self.wfule -(self.fbr * self.warp_dist)
-        return print(f"{self.wfule} units of warp fule remaneing")
-    def sustain_damege(self):
-        print("takeing damege")
+        return print(f"{self.wfule} units of warp fule remaneing.")
 
 class fighter(spaceship):
-    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, damege):
-        super().__init__(name, nfule, wfule, fbr, norm_dist, warp_dist,)
-        self.damege = damege
+    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, kills):
+        super().__init__(name, nfule, wfule, fbr, norm_dist, warp_dist)
+        self.kills = kills
     def attack(self):
-        target = input("Entre your target: ")
-        print("Attacking...")
+        target = input("What do you want to attack? e.g cx2213-alpha.")
+        print("Attaking target...")
+        time.sleep(1)
+        print(f"{target} was destroyed.")
+        self.kills = self.kills + 1
+
+class corvet(spaceship):
+    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, cargo, crew):
+        super().__init__(name, nfule, wfule, fbr, norm_dist, warp_dist)
+        self.cargo = cargo
+        self.crew = crew
+    def load(self):
+        cargo = input("what do you want to load in to the cargo-bay? ")
+        print("loading cargo...")
         time.sleep(1.5)
-        print(f"{target} has sustaind {self.damege} damege")
+        print(f"{cargo} was loades sucsefuly.")
 
 class transport(spaceship):
-    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, cargo):
+    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, pax):
         super().__init__(name, nfule, wfule, fbr, norm_dist, warp_dist)
-        self.cargo = cargo # expecting a list
-    def deliver_cargo(self):
-        if not self.cargo:
-            print("no cargo to deliver")
-        else:
-            print("delivering ", self.cargo[0])
-            self.cargo.pop(0)
-    def recive_cargo(self):
-        new_cargo = input("Entre what you want to add to your cargo: ")
-        print(F"{new_cargo} added to your cargo.")
-        self.cargo.append(new_cargo)
+        self.pax = pax
+    def board_pax(self):
+        pax = int(input("how many pasengers do you want to board?"))
+        print("boarding pasengers...")
+        self.pax = self.pax + pax
+        time.sleep(2)
+        print("all pasengers are aboard.")
 
-xf355 = fighter("bigg bessy", 1000, 200, 1.5, 100, 4, 50)
-xf355.attack()
+class science(spaceship):
+    def __init__(self, name, nfule, wfule, fbr, norm_dist, warp_dist, task):
+        super().__init__(name, nfule, wfule, fbr, norm_dist, warp_dist)
+        self.task = task
+    def reaserch(self):
+        task = input("what do you want to reasurch? e.g. nebula.")
+        print(f"traveling to reaserch sight...")
+        time.sleep(1.3)
+        print("arived at reasurch location, begining scans...")
+        time.sleep(1)
+        print("reasurch complete")
+        print("data added to archives")
+
+def create_fighter():
+    name = input("input the name of the fighter. e.g. sf109-bravo")
+    nfule = input("input the amount of normal fule")
+    wfule = input("input the amount of warp fule")
+    fbr = rand.randint(5, 10)
+    normd = 0
+    warpd = 0
+    kills = 0
+    ship = fighter(name, nfule, wfule, fbr, normd, warpd, kills)
+
+def create_corvet():
+    name = input("input the name of the fighter. e.g. sc011-oscar")
+    nfule = input("input the amount of normal fule")
+    wfule = input("input the amount of warp fule")
+    fbr = rand.randint(3, 7)
+    normd = 0
+    warpd = 0
+    cargo = "No cargo"
+    crew = int(input("pick a crew size from 2-6"))
+    if crew <2 or crew >6:
+        print("invalid input you must input an integer between 2 and 6")
+    else:
+        pass
+    ship = corvet(name, nfule, wfule, fbr, normd, warpd, cargo, crew)
+
+def create_transport():
+    name = input("input the name of the fighter. e.g. st209-yanky")
+    nfule = input("input the amount of normal fule")
+    wfule = input("input the amount of warp fule")
+    fbr = rand.randint(1, 5)
+    normd = 0
+    warpd = 0
+    pax = 0
+    ship = transport(name, nfule, wfule, fbr, normd, warpd, pax)
+
+def create():
+    print("what type of ship do you want to make? ")
+    print("1. fighter. quick & agile but a real gas guseler")
+    print("2. transport. very eficiant but slow and defenceless")
+    print("3. corvet. a hybrid of the other two with plenty of defenses but able to carry cargo and people, faster and more nimble than the transport but les fule eficiant")
+    inp = int(input())
+    if inp < 1 or inp > 3:
+        print("invalid input pleas enter either 1, 2 of 3")
+        create()
+    else:
+        if inp == 1:
+            create_fighter()
+        elif inp == 2:
+            create_transport()
+        elif inp == 3:
+            create_corvet()
